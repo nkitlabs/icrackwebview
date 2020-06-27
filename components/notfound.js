84f931fb7342955779fd2bcd88646ab833b04d35
NATTHAKUN KITTHAWORN <s89780@scbcorp.co.th>
2020-06-27 13:45:16 +0700

    change notFound Page

--- components/notfound.js
+++ components/notfound.js
@@ -1,6 +1,6 @@
 import React, { Component, useEffect, useState } from 'react'
 
-export default notFound() => {
+export default notFound = () => {
     return (
         <div>
             <span>404 - Not Found</span>
